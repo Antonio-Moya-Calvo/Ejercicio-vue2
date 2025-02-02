73a3f9fff7c520342d259bd636efebe2dfb193b0
Antonio <moyacalvo.cursos@gmail.com>
2025-02-02 21:04:47 +0100

    Ejercicio 5 Vue, Contador

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import TheTitle from '@/components/TheTitle.vue'
+import TheCounter from '@/components/TheCounter.vue';
 </script>
 <template>
     <TheTitle>Este Título te lo paso por Slot</TheTitle>
+    <TheCounter></TheCounter>
 </template>
---
+++ src/components/TheCounter.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import TheTitle from '@/components/TheTitle.vue'
+import { ref } from 'vue'
+
+const contador = ref(1)
+function increment() {
+  contador.value++
+}
+function decremet() {
+  contador.value--
+}
+</script>
+
+<template>
+  <section class="counter">
+    <TheTitle class="counter__title"
+      >Este contador puede cambiar su valor de forma dinámica</TheTitle
+    >
+    <p class="counter__value">{{ contador }}</p>
+    <p class="counter__increment">
+      <button @click="increment" class="counter__increment-button">
+        Incrementar el valor en 1
+      </button>
+    </p>
+    <p class="counter__decrement">
+      <button @click="decremet" class="counter__decrement-button">Decrementar el valor en 1</button>
+    </p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.counter {
+  width: fit-content;
+  margin: auto;
+  padding: 1em 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &__value {
+    font-size: var(--fs-big-medium);
+    color: var(--c-black);
+  }
+  &__increment {
+    margin: 2em 2em;
+  }
+  &__decrement {
+    margin: 2em 2em;
+  }
+}
+</style>
